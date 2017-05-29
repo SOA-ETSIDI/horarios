65541f304a09332d66d28cc5fade086b4f90ea65
Oscar Perpiñán <oscar.perpinan@gmail.com>
2017-05-29 09:46:38 +0200

    Mejora funcionamiento makeXLS

--- makeXLS.R
+++ makeXLS.R
@@ -2,7 +2,7 @@ library(openxlsx)
 
 source('init.R')
 
-makeXLS <- function(semestre, grupos = grupos, book = TRUE)
+makeXLS <- function(semestre, grupos = grupos, book = TRUE, path = tempdir())
 {
     old <- setwd('csv')
     on.exit(setwd(old))
@@ -18,7 +18,7 @@ makeXLS <- function(semestre, grupos = grupos, book = TRUE)
     empty <- sapply(dfs, function(x)nrow(x) == 0)
     dfs <- dfs[!empty]
 
-    if (!book)
+    if (!book) ## Todo junto en una misma hoja
     {
         dfs <- lapply(dfs, FUN = function(x)
             {
@@ -26,21 +26,34 @@ makeXLS <- function(semestre, grupos = grupos, book = TRUE)
                     x$Itinerario <- " "
                 x
             })
-        dfs <- do.call(merge, dfs, all = TRUE)
+        dfs <- do.call(rbind, dfs)
     }
     
-    xlsFile <- paste0('S', semestre, '.xlsx')
+    if (book)
+        xlsFile <- file.path(path, paste0('ETSIDI_S', semestre, '_grupos', '.xlsx'))
+    else
+        xlsFile <- file.path(path, paste0('ETSIDI_S', semestre, '.xlsx'))
+
     xls <- write.xlsx(dfs, file = xlsFile,
                       creator = 'ETSIDI',
                       asTable = TRUE)
-    lapply(seq_along(dfs), function(i)
+
+    if (book) lapply(seq_along(dfs), function(i)
     {
         setColWidths(xls, sheet = i,
                      cols = seq_len(ncol(dfs[[i]])),
                      widths = "auto")
     })
+    else
+        setColWidths(xls, sheet = 1,
+                     cols = seq_len(ncol(dfs)),
+                     widths = "auto")
+    
     saveWorkbook(xls, file = xlsFile, overwrite = TRUE)
 }
 
 makeXLS(1, grupos, book = FALSE)
-makeXLS(2, grupos)
+makeXLS(1, grupos, book = TRUE)
+
+makeXLS(2, grupos, book = FALSE)
+makeXLS(2, grupos, book = TRUE)
